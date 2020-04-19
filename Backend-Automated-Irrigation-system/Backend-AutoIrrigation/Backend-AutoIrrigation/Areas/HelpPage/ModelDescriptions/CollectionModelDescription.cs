e2fc9df036926948bd4eeb8760fdcfc5a0515769
Shashank Mishra <shashankmishra3003@gmail.com>
2020-04-19 16:16:23 -0700

    Initial Commit

---
+++ Backend-Automated-Irrigation-system/Backend-AutoIrrigation/Backend-AutoIrrigation/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Backend_AutoIrrigation.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
